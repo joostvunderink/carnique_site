06d0a07cbcd452b046794793ec96b1474ec05892
Joost Vunderink <joost@vunderink.net>
2014-06-15 10:21:28 +0200

    Added Top Quotes to navigation bar.
    Also made the quote title clickable.

--- carnique/quotes/urls.py
+++ carnique/quotes/urls.py
@@ -4,14 +4,12 @@ urlpatterns = patterns('',
     (r'^$', 'carnique.quotes.views.quote_view_page'),
     (r'^(?P<quote_id>[\d]+)/$', 'carnique.quotes.views.quote_view'),
     (r'^page/(?P<page_number>[\d]+)/$', 'carnique.quotes.views.quote_view_page'),
+    (r'^top/$', 'carnique.quotes.views.quote_view_top_page'),
     (r'^top/(?P<page_number>[\d]+)/$', 'carnique.quotes.views.quote_view_top_page'),
     (r'^random/$', 'carnique.quotes.views.quote_view_random'),
     (r'^add/$', 'carnique.quotes.views.quote_add'),
 
     # jquery interactive vote
     (r'^vote/$', 'carnique.quotes.views.quote_vote'),
-
-    (r'^(?P<quote_id>[\d]+)/vote/up/$', 'carnique.quotes.views.quote_vote_up'),
-    (r'^(?P<quote_id>[\d]+)/vote/down/$', 'carnique.quotes.views.quote_vote_down'),
 )
 
